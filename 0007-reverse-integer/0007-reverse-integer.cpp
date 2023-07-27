e5c64d1bcac6eebad49c6d793b7136dda7b680b8
Thanh <thanhthuynh7@gmail.com>
2023-07-27 14:44:30 -0400

    Time: 2 ms (51.8%), Space: 5.9 MB (89.05%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,7 +4,7 @@ public:
         int reverse = 0;
         while(x){
             if(reverse > INT_MAX/10 || reverse < INT_MIN/10) return 0;
-            reverse = reverse*10 + x % 10;
+            reverse = reverse*10 + x%10;
             x /= 10;
         }
         return reverse;
